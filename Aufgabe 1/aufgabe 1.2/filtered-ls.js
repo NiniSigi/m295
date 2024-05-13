84b2e64ad427e7b8974f29c8340f97941081e144
Nino Siegrist <nino.siegrist@netcetera.com>
2024-05-13 11:54:08 +0000

    working on Aufgabe 1

--- Aufgabe 1/aufgabe 1.2/filtered-ls.js
+++ Aufgabe 1/aufgabe 1.2/filtered-ls.js
@@ -1,14 +1,10 @@
-const fs = require('fs')
+const fs = require("fs");
 const dir = process.argv;
-fs.readdir(dir[2], 'utf8', function doneReading(err, data) {
-    if(err){
-        console.log(err)
-    }
-    for (let i = 0; i < data.length-1; i++) {
-        if(data[i].split('.').slice(1) == dir[3]){
-            
-        }
-        
-    }
-})
 
+fs.readdir(dir[2], "utf8", function doneReading(err, data) {
+  for (let i = 0; i < data.length - 1; i++) {
+    if (data[i].split(".").slice(1) == dir[3]) {
+      console.log(data[i]);
+    }
+  }
+});
