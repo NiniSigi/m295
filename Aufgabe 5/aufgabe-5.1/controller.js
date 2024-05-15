60c71ca2ce9fb563cb72251f3edc6a8be7dce8c5
Nino Siegrist <nino.siegrist@netcetera.com>
2024-05-15 08:41:05 +0000

    add lends

--- Aufgabe 5/aufgabe-5.1/controller.js
+++ Aufgabe 5/aufgabe-5.1/controller.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 const app = express();
 const port = 3000;
 
 let books = [
   {"isbn": "978-3-522-46051-4", "title": "Momo", "author": "Michael Ende", "year": 2023},
   {"isbn": "978-3-16-148410-0", "title": "Der Herr der Ringe", "author": "J.R.R. Tolkien", "year": 1954},
-  {"isbn": "978-3-446-13470-4", "title": "Harry Potter und der Stein der Weisen", "author": "J.K. Rowling", "year": 1997},
+  {"isbn": "", "title": "Harry Potter und der Stein der Weisen", "author": "J.K. Rowling", "year": 1997},
   {"isbn": "978-3-423-20125-3", "title": "Die Verwandlung", "author": "Franz Kafka", "year": 1915},
   {"isbn": "978-3-15-009381-1", "title": "Faust", "author": "Johann Wolfgang von Goethe", "year": 1808},
   {"isbn": "978-3-15-010818-8", "title": "Der Kleine Prinz", "author": "Antoine de Saint-Exupéry", "year": 1943},
@@ -13,6 +14,15 @@ let books = [
   {"isbn": "978-3-499-22620-1", "title": "Der Steppenwolf", "author": "Hermann Hesse", "year": 1927},
   {"isbn": "978-3-312-00387-6", "title": "Der Alchimist", "author": "Paulo Coelho", "year": 1988},
   {"isbn": "978-3-453-29115-6", "title": "Die Unendliche Geschichte", "author": "Michael Ende", "year": 1979}
+];
+let lends=[
+  {
+    "id": "2f5f028d-0e32-4e0d-bf8a-0cc4b5b63c82",
+    "customer_id": "1",
+    "isbn": "978-3-522-46051-4",
+    "borrowed_at": "2024-05-14T08:00:00Z",
+    "returned_at": null
+  }  
 ];
 
 app.use(express.json());
@@ -41,6 +51,34 @@ app.delete('/books/:isbn',(request, response) => {
   response.send(books)
 });
 
+app.patch('/books/:isbn', (request, response) => {
+  const isbn = request.params;
+  const updatedBook = request.body;
+  books = books.map((book) => (book.isbn === isbn ? { ...book, ...updatedBook } : book));
+  response.send(books);
+});
+
+app.get('/lends', (request, response) => {
+  response.send(lends);
+});
+
+app.get('/lends/:id', (request, response) => {
+  response.send(lends.find((lend) => lend.id === request.params.id));
+});
+
+app.post('/lends', (request, response) =>{
+  const completBook = {...request.body, id: uuidv4(), borrowed_at: new Date().toLocaleString('de-CH'), returned_at: null}
+  lends = [...lends, completBook]
+  response.send(completBook)
+})
+
+app.delete('/lends/:id',(request, response) => {
+  const lend = lends.find((lend) => lend.id === request.params.id)
+  const lendReturned = {...lend, returned_at: new Date().toLocaleString('de-CH')}
+  console.log(lendReturned)
+  lends = lends.map((book) => (book.id === request.params.id ? { ...book, ...lendReturned } : book));
+  response.send(lends)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
